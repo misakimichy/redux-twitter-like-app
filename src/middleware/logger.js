7d0160b5527c25a2a0c7bb54763fc1e0550013ea
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-21 19:20:59 -0700

    Fix: Fix typo.

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -4,7 +4,7 @@ const logger = store => next => action => {
         const returnValue = next(action)
         console.log('The new state: ', store.getState())
     console.groupEnd()
-    return returnvalue
+    return returnValue
 }
 
 export default logger;
