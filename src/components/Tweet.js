e081ed0772401bf179556fd8ae4252ec0295ef39
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-24 12:29:37 -0700

    Feat: Update Tweet component.

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,14 @@ import { formatTweet } from '../utils/helpers';
 
 class Tweet extends Component {
     render() {
+        const { tweet } = this.props;
+
+        if (tweet === null) {
+            return(
+                <p>This tweet doesn't exist.</p>
+            )
+        }
+
         console.log(this.props);
         return (
             <div className='tweet'>
@@ -14,10 +22,13 @@ class Tweet extends Component {
 }
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
-    const tweet = tweets[id]
+    const tweet = tweets[id];
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet
+        ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+        : null
     }
 }
 
