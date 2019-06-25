e497797bca1c69cec417772cd7783e4868035872
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-24 20:56:58 -0700

    Fix: Update statement error.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -13,7 +13,7 @@ export default function tweets (state = {}, action) {
             ...state,
             [action.id]: {
                 ...state[action.id],
-                likes: action.hasLiked
+                likes: action.hasLiked === true
                 ? state[action.id].likes.filter(uid => uid !== action.authedUser)
                 : state[action.id].likes.concat([action.authedUser])
             }
