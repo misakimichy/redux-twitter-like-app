5d471ad2e700070aa54bb594103d5f5bb42f26ce
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-25 18:48:02 -0700

    Feat: Render NewTweet component instead of Dashboard.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
-import LoadingBar from 'react-redux-loading-bar'
+import LoadingBar from 'react-redux-loading-bar';
+import NewTweet from './NewTweet';
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +15,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading
           ? null
-          : <Dashboard />
+          : <NewTweet />
         }
       </div>
     )
