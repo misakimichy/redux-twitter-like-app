1a48010fbdc6d314271915bdeda27a106c74ac69
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-26 10:45:44 -0700

    Feat: Organize import section.

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { handleAddTweet } from '../actions/tweets';
 import { Redirect } from 'react-router-dom';
+import { handleAddTweet } from '../actions/tweets';
 
 class NewTweet extends Component {
     state= {
