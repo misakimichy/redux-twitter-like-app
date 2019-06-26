1fe4286f97bcfbb027cb3a11e0751e1e6bc4d02a
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-25 21:09:38 -0700

    Feat: Add Router and Route path.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import LoadingBar from 'react-redux-loading-bar';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
-import LoadingBar from 'react-redux-loading-bar';
 import NewTweet from './NewTweet';
 import TweetPage from './TweetPage';
+import Nav from './Nav';
 
 class App extends Component {
   componentDidMount() {
@@ -12,13 +14,23 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading
-          ? null
-          : <TweetPage match={{params:{id: '8xf0y6ziyjabvozdd253nd'}}}/>
-        }
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/tweet:id' component={TweetPage} />
+                  <Route path='/new' component={NewTweet} />
+                </div>
+            }
+          </div>
+        </Fragment>
+      </Router>
+
     )
   }
 }
