74a533f9fdd6bc190adbce32bfedea1f961629f3
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-25 19:00:41 -0700

    Feat: Add compose new tweet section.

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -28,9 +28,33 @@ class NewTweet extends Component {
     }
 
     render() {
+        const { text } = this.state;
+        // Todo: redirect to '/' when submitted
+        const tweetLeft = 280 - text.length;
         return (
             <div>
-                New Tweet
+                <h3 className='center'>Compose new Tweet</h3>
+                <form className='new-tweet' onSubmit={this.handleSubmit}>
+                    <textarea
+                        className='textarea'
+                        placeholder="What's happening?"
+                        value={text}
+                        onChange={this.handleChange}
+                        maxLength={280}
+                    />
+                    {tweetLeft <= 100 && (
+                        <div className='tweet-length'>
+                            {tweetLeft}
+                        </div>
+                    )}
+                    <button
+                        className='button'
+                        type='submit'
+                        disabled={text === ''}
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         )
     }
