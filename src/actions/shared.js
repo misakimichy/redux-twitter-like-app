bb1713eb6b1edafb65d718ea04fdd0088285b419
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-26 21:29:32 -0700

    Fix: Fix the authed_ID.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,7 +4,7 @@ import { receiveTweets } from '../actions/tweets';
 import { setAuthedUser } from '../actions/authedUser';
 import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
-const AUTHED_ID = 'tylermacginnis';
+const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData () {
     return (dispatch) => {
